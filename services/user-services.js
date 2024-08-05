b0f66ccfc14ab73965257f2ca5457c18a617a5b5
Hamoud Mehanna <hamoud@alchemi-data.com>
2024-08-05 05:05:38 -0400

    fix: promise issue fixed

--- services/user-services.js
+++ services/user-services.js
@@ -7,22 +7,21 @@ class UserServices {
         return this.userRepository.findAll();
     }
 
-    createUser(data) {
+    async createUser(data) {
         const email = data.email;
-        if (this.getUserByEmail(email)) {
+        if (await this.getUserByEmail(email)) {
             throw new Error("User already exists");
         }
-
         return this.userRepository.create(data);
     }
 
     async getUserByEmail(email) {
         try {
-            await this.userRepository.findByEmail(email);
-            return true;
+             const user = await this.userRepository.findByEmail(email);
+            return !!user;
         } catch (err) {
             console.log(err);
-            return false
+            return false;
         }
     }
 }
